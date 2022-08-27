a32de8ae876babc9a4b89bd7145e983c412ee6e6
Robert Teets <teetsrobert@gmail.com>
2022-08-26 20:56:55 -0600

    assignment tasks completed

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,11 +1,28 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from flask_app import app
+from flask_app.models.users import User
 
 
 @app.route('/users')
 def r_users():
-    return render_template('users.html')
+    users = User.get_all()
+    return render_template('users.html', users=users)
+
 
 @app.route('/users/create')
 def r_create():
     return render_template('create.html')
+
+
+@app.route('/users/add_user', methods=['POST'])
+def add_user():
+    inbound = request.form
+    data = {
+        'first_name' : inbound['first_name'],
+        'last_name' : inbound['last_name'],
+        'email' : inbound['email_address'],
+    }
+
+    User.create(data)
+
+    return redirect('/users')
